09e7e7d9117e9f279a9b7660db174d97270fe565
Nevena Simić <nevena_simic@outlook.com>
2016-12-30 02:05:42 +0100

    Sort DataFrame

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 # number of rows from ratings.dat file which will be processed
 NUMBER_OF_ROWS = 100000
@@ -8,19 +9,23 @@ NUMBER_OF_ROWS = 100000
 columns = ['UserID', 'MovieID', 'Rating']
 # read data
 data = pandas.read_csv('./data/ml-10M100K/ratings.dat', sep='::', engine='python', header=None, names=columns, usecols=columns, nrows=NUMBER_OF_ROWS)
-# transform data
-data = data.pivot(index=columns[0], columns=columns[1], values=columns[2])
-# replace missing values with zeros
-data = data.fillna(0)
+data = data.pivot(index=columns[0], columns=columns[1], values=columns[2])  # transform data
+data = data.fillna(0)  # replace missing values with zeros
 
-# heat-map before matrix sort
+# heat-map before DataFrame sort
 sns.heatmap(data, yticklabels=False, xticklabels=False)
 plt.savefig('./images/heatmap-before-sort.png')
 plt.clf()
 
-# sort matrix
-# TODO: Sort user-item matrix so that the density decreases along the main diagonal
+# Sort user-item DataFrame so that the density decreases along the main diagonal
+a = data.values
+a.sort(axis = 1)
+a = a[:, ::-1]
+a.sort(axis = 0)
+a = a[::-1, :]
+data = pandas.DataFrame(a, data.index, data.columns)
+print data
 
-# heat-map after matrix sort
+# heat-map after DataFrame sort
 sns.heatmap(data, yticklabels=False, xticklabels=False)
 plt.savefig('./images/heatmap-after-sort.png')
