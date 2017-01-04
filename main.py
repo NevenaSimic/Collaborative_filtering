390d1fa10724195626f664411fa64fa5ed08c066
Nevena Simić <nevena_simic@outlook.com>
2017-01-04 01:36:10 +0100

    Create matrix with UserID and number of votes

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pandas
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 import model_based
+import memory_based
 
 # number of rows from ratings.dat file which will be processed
 NUMBER_OF_ROWS = 10000
@@ -26,8 +28,14 @@ for i in range(0, matrix_data.shape[0]-1):
     row = matrix_data[i]
     no_of_votes = sum(k > 0 for k in row)
     print "The user with id: ", i+1, " voted ", no_of_votes, " times."
-# TODO sort
+# Create matrix with UserID and number of votes
+    Num_users, Num_votes = 84, 2976
+    temp_matrix = np.ones((Num_users, Num_votes))
+    temp_matrix = ([[i+1], [no_of_votes]])
+    print temp_matrix
+# TODO Sort matrix
 
+#TODO Back to data frame
 
 # heat-map after DataFrame sort
 sns.heatmap(data, yticklabels=False, xticklabels=False)
@@ -45,4 +53,3 @@ print 'K-means labels: ', kmeans_cluster.labels_, '\n'
 
 agglomerative_cluster = model_based.agglomerative_clustering(data)
 print 'Agglomerative labels: ', agglomerative_cluster.labels_, '\n'
-
