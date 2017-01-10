c82369495a174983484de31676f93f09c15c0911
Nevena Simić <nevena_simic@outlook.com>
2017-01-10 17:44:22 +0100

    Sorted user-item matrix so that the density decreases along the main diagonal

--- main.py
+++ main.py
@@ -21,33 +21,24 @@ sns.heatmap(data, yticklabels=False, xticklabels=False)
 plt.savefig('./images/heatmap-before-sort.png')
 plt.clf()
 
-# Data frame to matrix
+# DataFrame to matrix
 matrix_data = data.as_matrix()
 # Number of votes for each user
 matrix = np.zeros((matrix_data.shape[0], 2))
-for i in range(0, matrix_data.shape[0]-1):
+for i in range(0, matrix_data.shape[0]):
     row = matrix_data[i]
     no_of_votes = sum(k > 0 for k in row)
-    #print "The user with id: ", i+1, " voted ", no_of_votes, " times."
-
-# Create matrix with UserID and number of votes
-    matrix[i][0] = i+1
+    #print "The user with id: ", data.index.values[i], " voted ", no_of_votes, " times."
+    # Create matrix with UserID and number of votes
+    matrix[i][0] = data.index.values[i]
     matrix[i][1] = no_of_votes
-print "*********Original matrix********"
-print matrix
-print "********************************\n\n"
-print "*********Sorted matrix**********"
+
+# Sorted matrix with UserID and number of votes
 matrix = matrix[matrix[:, 1].argsort()[::-1]]
-print matrix
-print "********************************\n\n"
-
-#Back to data frame
-matrix_column = matrix[:, 0]
-data = pandas.DataFrame(matrix_data)
-data = data.set_index(matrix_column)
-print data
-
-#TODO Sort values
+matrix_column = np.asarray(matrix[:, 0], dtype= np.int32)
+# Sorted user-item matrix so that the density decreases along the main diagonal
+data = data.reindex(matrix_column)
+print data, '\n'
 
 # heat-map after DataFrame sort
 sns.heatmap(data, yticklabels=False, xticklabels=False)
@@ -59,7 +50,6 @@ print 'Number of items:', len(data.columns), '\n'
 
 # Memory-based algorithms
 num_users = matrix_data.shape[0]
-print "\n\n**********************************\n\n"
 for user1 in range(num_users):
     for user2 in range(num_users):
         if user1 is not user2:
