679c140c66844e489b530e876b5815e67f7765d1
Zoran Luledzija <zoran.luledzija@outlook.com>
2017-02-06 16:31:03 +0100

    improved matrix sort

--- sort.py
+++ sort.py
@@ -3,21 +3,26 @@ import numpy as np
 
 
 def sort(data):
-    matrix_data = data.as_matrix()
-    # Number of votes for each user
-    matrix = np.zeros((matrix_data.shape[0], 2))
-    for i in range(0, matrix_data.shape[0]):
-        row = matrix_data[i]
-        no_of_votes = sum(k > 0 for k in row)
-        # print "The user with id: ", data.index.values[i], " voted ", no_of_votes, " times."
-        # Create matrix with UserID and number of votes
-        matrix[i][0] = data.index.values[i]
-        matrix[i][1] = no_of_votes
+    n, m = data.shape
 
-    # Sorted matrix with UserID and number of votes
+    # sort by rows
+    matrix_data = data.as_matrix()
+    matrix = np.zeros((n, 2), dtype=np.int32)
+    for i in range(n):
+        matrix[i][0] = data.index.values[i]
+        matrix[i][1] = sum(k > 0 for k in matrix_data[i])
+
     matrix = matrix[matrix[:, 1].argsort()[::-1]]
-    matrix_column = np.asarray(matrix[:, 0], dtype=np.int32)
-    # Sorted user-item matrix so that the density decreases along the main diagonal
-    data = data.reindex(matrix_column)
+    data = data.reindex(np.asarray(matrix[:, 0], dtype=np.int32))
+
+    # sort by cols
+    matrix_data = data.as_matrix()
+    matrix = np.zeros((m, 2), dtype=np.int32)
+    for i in range(m):
+        matrix[i][0] = data.columns.values[i]
+        matrix[i][1] = sum(k > 0 for k in matrix_data[:, i])
+
+    matrix = matrix[matrix[:, 1].argsort()[::-1]]
+    data = data.reindex(columns=np.asarray(matrix[:, 0], dtype=np.int32))
 
     return data
