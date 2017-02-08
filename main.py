6389f5cee0c5515e0d2df1d71f67d66731d65895
Zoran Luledzija <zoran.luledzija@outlook.com>
2017-02-08 13:25:16 +0100

    memory-based improvements

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from math import sqrt
 from time import time
 import pandas as pd
-import numpy as np
 from sort import sort
 from helpers import *
 import model_based
 import memory_based
-from operator import itemgetter
-from scipy.spatial import distance
-from sklearn.neighbors import NearestNeighbors
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 # number of rows from ratings.dat file which will be processed
@@ -38,61 +34,54 @@ user_based_data = sort(user_based_data)
 save_heatmap_image(user_based_data, 'user-based-heatmap-after-sort')
 
 
-###########################
-# Memory-based algorithms #
-###########################
-print '*******************************************'
-print '***  MEMORY-BASED(User-based approach)  ***'
-print '*******************************************', '\n'
-
-nbrs_euclidean = NearestNeighbors(algorithm='brute', metric='euclidean')
-nbrs_euclidean.fit(user_based_data)
-query_index = np.random.choice(user_based_data.shape[0])
-distances, indices = nbrs_euclidean.kneighbors(user_based_data.iloc[query_index, :].reshape(1, -1), n_neighbors=6)
-for i in range(0, len(distances.flatten())):
-    if i == 0:
-        print 'Recommendations(EUCLIDEAN DISTANCE) for {0}:\n'.format(user_based_data.index[query_index])
-    else:
-        print '{1}, with distance of {2}:'.format(i, user_based_data.index[indices.flatten()[i]],distances.flatten()[i])
-
-print '\n'
-
-nbrs_cosine = NearestNeighbors(algorithm='brute', metric = 'cosine')
-nbrs_cosine.fit(user_based_data)
-query_index = np.random.choice(user_based_data.shape[0])
-distances, indices = nbrs_cosine.kneighbors(user_based_data.iloc[query_index, :].reshape(1, -1), n_neighbors = 6)
-for i in range(0, len(distances.flatten())):
-    if i == 0:
-        print 'Recommendations(COSINE SIMILARITY) for {0}:\n'.format(user_based_data.index[query_index])
-    else:
-        print '{1}, with distance of {2}:'.format(i, user_based_data.index[indices.flatten()[i]],distances.flatten()[i])
-
-print '\n'
-
-nbrs_jaccard = NearestNeighbors(algorithm='brute', metric='jaccard')
-nbrs_jaccard.fit(user_based_data)
-query_index = np.random.choice(user_based_data.shape[0])
-distances, indices = nbrs_jaccard.kneighbors(user_based_data.iloc[query_index, :].reshape(1, -1), n_neighbors=6)
-for i in range(0, len(distances.flatten())):
-    if i == 0:
-        print 'Recommendations(JACCARD SIMILARITY) for {0}:\n'.format(user_based_data.index[query_index])
-    else:
-        print '{1}, with distance of {2}:'.format(i, user_based_data.index[indices.flatten()[i]],distances.flatten()[i])
-
-
-##########################
-# Model-based algorithms #
-##########################
-print '*******************************************'
-print '***             MODEL-BASED             ***'
-print '*******************************************', '\n'
-
 # predict ratings for the given user and movies
 user_id = 34
-movies = [593, 110, 50, 457]
-true_ratings = user_based_data.loc[34, movies].values.tolist()
+movies = [593, 110, 50, 457, 1, 150, 608, 377, 296, 356]
+true_ratings = user_based_data.loc[user_id, movies].values.tolist()
 
-# base kmeans cluster recommendation
+
+#############################
+#  Memory-based algorithms  #
+#############################
+# user-based
+start = time()
+predicted_ratings = memory_based.euclidean_knn_recommendation(user_based_data, user_id, movies, n=6)
+execution = time() - start
+mae = mean_absolute_error(true_ratings, predicted_ratings)
+rmse = sqrt(mean_squared_error(true_ratings, predicted_ratings))
+print 'Euclidean kNN recommendation:'
+print 'True ratings: {}\nPredicted ratings: {}\nMAE: {}\nRMSE: {}\nExecution: {}\n'.format(true_ratings, predicted_ratings, mae, rmse, execution)
+
+start = time()
+predicted_ratings = memory_based.jaccard_knn_recommendation(user_based_data, user_id, movies, n=6)
+execution = time() - start
+mae = mean_absolute_error(true_ratings, predicted_ratings)
+rmse = sqrt(mean_squared_error(true_ratings, predicted_ratings))
+print 'Jaccard kNN recommendation:'
+print 'True ratings: {}\nPredicted ratings: {}\nMAE: {}\nRMSE: {}\nExecution: {}\n'.format(true_ratings, predicted_ratings, mae, rmse, execution)
+
+start = time()
+predicted_ratings = memory_based.cosine_knn_recommendation(user_based_data, user_id, movies, n=6)
+execution = time() - start
+mae = mean_absolute_error(true_ratings, predicted_ratings)
+rmse = sqrt(mean_squared_error(true_ratings, predicted_ratings))
+print 'Cosine kNN recommendation:'
+print 'True ratings: {}\nPredicted ratings: {}\nMAE: {}\nRMSE: {}\nExecution: {}\n'.format(true_ratings, predicted_ratings, mae, rmse, execution)
+
+start = time()
+predicted_ratings = memory_based.pearson_knn_recommendation(user_based_data, user_id, movies, n=6)
+execution = time() - start
+mae = mean_absolute_error(true_ratings, predicted_ratings)
+rmse = sqrt(mean_squared_error(true_ratings, predicted_ratings))
+print 'Pearson kNN recommendation:'
+print 'True ratings: {}\nPredicted ratings: {}\nMAE: {}\nRMSE: {}\nExecution: {}\n'.format(true_ratings, predicted_ratings, mae, rmse, execution)
+
+# item-based
+# TODO: implement
+
+#############################
+#   Model-based algorithms  #
+#############################
 start = time()
 predicted_ratings = model_based.base_kmeans_cluster_recommendation(user_based_data, user_id, movies)
 execution = time() - start
@@ -101,7 +90,6 @@ rmse = sqrt(mean_squared_error(true_ratings, predicted_ratings))
 print 'Base KMeans cluster recommendation:'
 print 'True ratings: {}\nPredicted ratings: {}\nMAE: {}\nRMSE: {}\nExecution: {}\n'.format(true_ratings, predicted_ratings, mae, rmse, execution)
 
-# knn kmeans cluster recommendation
 start = time()
 predicted_ratings = model_based.kmeans_cluster_recommendation(user_based_data, user_id, movies, 6)
 execution = time() - start
@@ -110,7 +98,6 @@ rmse = sqrt(mean_squared_error(true_ratings, predicted_ratings))
 print 'KNN KMeans cluster recommendation:'
 print 'True ratings: {}\nPredicted ratings: {}\nMAE: {}\nRMSE: {}\nExecution: {}\n'.format(true_ratings, predicted_ratings, mae, rmse, execution)
 
-# base agglomerative cluster recommendation
 start = time()
 predicted_ratings = model_based.base_agglomerative_cluster_recommendation(user_based_data, user_id, movies)
 execution = time() - start
@@ -119,7 +106,6 @@ rmse = sqrt(mean_squared_error(true_ratings, predicted_ratings))
 print 'Base Agglomerative cluster recommendation:'
 print 'True ratings: {}\nPredicted ratings: {}\nMAE: {}\nRMSE: {}\nExecution: {}\n'.format(true_ratings, predicted_ratings, mae, rmse, execution)
 
-# knn agglomerative cluster recommendation
 start = time()
 predicted_ratings = model_based.agglomerative_cluster_recommendation(user_based_data, user_id, movies, 6)
 execution = time() - start
--- memory_based.py
+++ memory_based.py
@@ -1,9 +1,59 @@
 # author: Nevena Simic
-from math import sqrt
+from rating_predictions import similarity_rating_prediction
+from sklearn.neighbors import NearestNeighbors
 
-def euclidean(rating1, rating2):
-	distance = 0
-	for key in rating1:
-		if key in rating2:
-			distance += pow(rating1[key] - rating2[key], 2)
-	return sqrt(distance)
+
+def euclidean_knn_recommendation(data, user_id, movies, n=5):
+    nn = NearestNeighbors(algorithm='brute', metric='euclidean', n_neighbors=n)
+    nn.fit(data)
+    distances, indices = nn.kneighbors(data.ix[user_id, :].values.reshape(1, -1))
+    distances = distances.tolist()[0]
+    indices = indices.tolist()[0]
+
+    ratings = []
+    for movie_id in movies:
+        ratings.append(similarity_rating_prediction(data.iloc[indices, :], distances, movie_id))
+
+    return ratings
+
+
+def jaccard_knn_recommendation(data, user_id, movies, n=5):
+    nn = NearestNeighbors(algorithm='brute', metric='jaccard', n_neighbors=n)
+    nn.fit(data)
+    distances, indices = nn.kneighbors(data.ix[user_id, :].values.reshape(1, -1))
+    distances = distances.tolist()[0]
+    indices = indices.tolist()[0]
+
+    ratings = []
+    for movie_id in movies:
+        ratings.append(similarity_rating_prediction(data.iloc[indices, :], distances, movie_id))
+
+    return ratings
+
+
+def cosine_knn_recommendation(data, user_id, movies, n=5):
+    nn = NearestNeighbors(algorithm='brute', metric='cosine', n_neighbors=n)
+    nn.fit(data)
+    distances, indices = nn.kneighbors(data.ix[user_id, :].values.reshape(1, -1))
+    distances = distances.tolist()[0]
+    indices = indices.tolist()[0]
+
+    ratings = []
+    for movie_id in movies:
+        ratings.append(similarity_rating_prediction(data.iloc[indices, :], distances, movie_id))
+
+    return ratings
+
+
+def pearson_knn_recommendation(data, user_id, movies, n=5):
+    nn = NearestNeighbors(algorithm='brute', metric='correlation', n_neighbors=n)
+    nn.fit(data)
+    distances, indices = nn.kneighbors(data.ix[user_id, :].values.reshape(1, -1))
+    distances = distances.tolist()[0]
+    indices = indices.tolist()[0]
+
+    ratings = []
+    for movie_id in movies:
+        ratings.append(similarity_rating_prediction(data.iloc[indices, :], distances, movie_id))
+
+    return ratings
